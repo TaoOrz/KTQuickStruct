d898702270e2efeec38bb5debe6abceb6451b81c
TaoOrz <33273954+TaoOrz@users.noreply.github.com>
2024-12-06 16:55:08 +0800

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AULibrary",
+    platforms: [.iOS(.v14),.macOS(.v10_15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(name: "AULibrary", targets: ["AULibrary"]),
+        .library(name: "AULibraryManager", targets: ["AULibraryManager"])
+    ],
+    dependencies: [
+            // 在此列出你的依赖项
+        .package(
+          url: "https://github.com/SnapKit/SnapKit",
+          "5.0.1" ..< "5.7.1"
+        ),
+        .package(
+          url: "https://github.com/onevcat/Kingfisher",
+          "8.0.0" ..< "8.1.1"
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "AULibrary",
+          path: "./Sources/AULibrary.xcframework"),
+        .target(name: "AULibraryManager",dependencies: ["SnapKit","Kingfisher"])
+    ]
+)
---
+++ Sources/AULibrary.xcframework/ios-arm64/AULibrary.framework/Headers/AULibrary.h
@@ -0,0 +1,18 @@
+//
+//  AULibrary.h
+//  AULibrary
+//
+//  Created by mac on 2024/12/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AULibrary.
+FOUNDATION_EXPORT double AULibraryVersionNumber;
+
+//! Project version string for AULibrary.
+FOUNDATION_EXPORT const unsigned char AULibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AULibrary/PublicHeader.h>
+
+
